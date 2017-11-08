0c3f2c981b891c35a771c8bb9451b8965e6770da
Bird-e <bird2git@gmail.com>
2017-11-08 15:28:09 -0500

    Added comments to my code

--- JS/JSClass11_Assign1/class11_a1.js
+++ JS/JSClass11_Assign1/class11_a1.js
@@ -1,5 +1,9 @@
+//Get a name from the user
 var input = prompt("Enter your name, please");
-
+//Target the html h1 with the id of 'name'
+//Add the text 'The name of your Dinosaur is:' plus the name provided by the user
 document.getElementById('name').innerHTML = "The name of your Dinosaur is: " + input;
+//Targets the element with the id of 'name'
 var changeColor = document.getElementById('name');
+//Change the color of the text of 'name'
 changeColor.style.color = prompt('Give me a color');
