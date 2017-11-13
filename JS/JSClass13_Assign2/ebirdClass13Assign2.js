2822a97a5ffe48931cf4adac8e34cde15f64ce6b
Bird-e <bird2git@gmail.com>
2017-11-13 16:09:48 -0500

    updated assignment to proper peramaters.

--- JS/JSClass13_Assign2/ebirdClass13Assign2.js
+++ JS/JSClass13_Assign2/ebirdClass13Assign2.js
@@ -1,10 +1,15 @@
-var fruit = [];
-
-var input = prompt('Enter a fruit please:');
-fruit.unshift(input);
-var input2 = prompt('Enter a fruit please:');
+//created an array of fruit
+var fruit = ["apple", "orange", "kiwi"];
+//Get more fruits from the user and puts them at the end of the array. Makes them all lowercase
+var input = prompt('Add more fruit').toLowerCase();
+fruit.push(input);
+var input2 = prompt('Add more fruit').toLowerCase();
 fruit.push(input2);
-var input3 = prompt('Enter a fruit please:');
+var input3 = prompt('Add more fruit').toLowerCase();
 fruit.push(input3);
-
-document.getElementById('fruitList').innerHTML = "Your fruit shopping list is: " + fruit.join(" and a(n) ");
+//Shows the user the original array, unsorted
+document.getElementById('original').innerHTML = "Your original list is: " + fruit.join(", ");
+//sorts the array by alphabetical orders
+fruit.sort();
+//outputs the array in the h1 with the id of 'fruitList' shows the list in alphabetical order seperated by 'with'
+document.getElementById('fruitList').innerHTML = "Your alphbetically sorted fruit salad will have: " + fruit.join(" with ") +".";
