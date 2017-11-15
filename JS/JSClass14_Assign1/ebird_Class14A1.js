0dcf1377f4662c18cb7021045999809d2617e4b9
Bird-e <bird2git@gmail.com>
2017-11-15 14:33:40 -0500

    Revising assignment with comments

--- JS/JSClass14_Assign1/ebird_Class14A1.js
+++ JS/JSClass14_Assign1/ebird_Class14A1.js
@@ -1,17 +1,23 @@
+//Array for display
 var allNums = [];
+//User Input
 var num1 = parseInt(prompt('Please type a number.'));
 var num2 = parseInt(prompt('Please type a number.'));
 var num3 = parseInt(prompt('Please type a number.'));
+//Adding user input to array
 allNums.push(num1);
 allNums.push(num2);
 allNums.push(num3);
 
+//Displaying array in order of first to last
 document.getElementById('list').innerHTML = "Your original numbers are: " + allNums.join(", ");
-
+//if the first number is greater than both the second and the third, say the first is the highest number
 if(num1 > num2 && num1 > num3){
 	document.getElementById('compare').innerHTML = "Your first choice, number: " + num1 + " is the highest number."
 }else if(num2 > num1 && num2 > num3){
+	//if the second number is greater than both the first and the third, say the second is the highest number
 	document.getElementById('compare').innerHTML = "Your second choice, number: " + num2 + " is the highest number."
 }else{
+	//if the first and second numbers are not greater than the third, say the third is the highest number
 	document.getElementById('compare').innerHTML = "Your third choice, number: " + num3 + ", is the highest number."
 }
